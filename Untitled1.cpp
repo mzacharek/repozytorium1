abdf42389b2d39961c57d2904329b7f8d9ba4392
Michalina Zacharek <michalina.zacharek@wp.pl>
2026-06-03 11:38:10 +0200

    Dodalam monety do plaanszy i ogolnie ja dokonczylam

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -5,5 +5,16 @@ int main(){
     int n=10;
     char mapa[n][n];
 
+    for (int i=0; i<n; i++)
+        for(int j=0; j<n; i++)
+            mapa[i][j]='.';
+  mapa[1][2]='M';
+  mapa[3][4]='M';
+  mapa[4][1]='M';
+  mapa[5][7]='M';
+  mapa[6][7]='M';
+  mapa[9][8]='M';
 
+  int punkty;
+  int x=0, y=0;
 }
